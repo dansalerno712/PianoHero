6021d6b762b7118a75305abe49a2d6e20da5492c
Dan Salerno <dsalern3@its.jnj.com>
2017-11-18 13:06:20 -0500

    better better device grabbing

--- pianoHero.py
+++ pianoHero.py
@@ -10,10 +10,13 @@ def main():
     pygame.init()
     midi.init()
 
+    devNum = 0
     for m in range(midi.get_count()):
         device = midi.get_device_info(m)
-        if device[1] == "AKM320 MIDI 1" and device[2] == 0:
+        print(device, device[1], device[2])
+        if "AKM320" in str(device[1]) and device[2] == 1:
             devNum = m
+            print(devNum)
 
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption("Piano Hero")
